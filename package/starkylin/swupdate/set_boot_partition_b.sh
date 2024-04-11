a9088564513e7afb860748ffcad60630b784618b
Chanj Wons <wangchangqi@kylinos.cn>
2024-04-11 15:06:12 +0800

    ota-autoboot-solution

---
+++ package/starkylin/swupdate/set_boot_partition_b.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# By JMVA, VIDAL & ASTUDILLO Ltda, 2022
+# www.vidalastudillo.com
+
+#
+# Updates the autoboot.txt to tell a RaspberryPi to boot from the partition A
+# 
+
+echo "[all]
+tryboot_a_b=1
+boot_partition=3
+[tryboot]
+boot_partition=2
+" > '/boot/autoboot.txt'
+echo "Next reboot will load from: boot_b"
